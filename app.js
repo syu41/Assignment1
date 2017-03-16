558c618229426171360d74813e9c351dbf5e5674
syu41 <syu41@student.monash.edu>
2017-03-16 23:01:18 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+    console.log("Connected");
+})
+server.listen(8080, function() {
+    console.log('Listening at: http://localhost:8080');
+});
+
+
+
+// create a mew board 
+var five = require("johnny-five");
+var led;
+var motion;
+var board = new five.Board();
+board.on("ready", function() {
+    // create a new 'LED' instance
+    led = new five.Led(13);
+    // create a new `motion` hardware instance
+    motion = new five.Motion(7);
+});
+
+
+
+// create socket
+var socket = require('socket.io').listen(server);
+socket.on('connection', function(socket) {
+    // if client requests to turn motion sensor
+    socket.on("turn motion sensor on", function() {
+        console.log('motion sensor on');
+        
+        //create variable to determine motion duration
+        var stime,
+        etime,
+        duration
+
+        // "calibrated" occurs once, at the beginning of a session
+        motion.on("calibrated", function() {
+            console.log("calibrated");
+        });
+            
+        motion.on('motionstart', function() {
+            // record time of motion detected
+            led.on();
+            console.log('motion detected')
+            stime = Date.now();
+        });
+
+        motion.on('motionend', function() {
+            // calculate motion duration
+            led.off();
+            console.log('motion ended')
+            etime = Date.now();
+            duration = etime - stime;
+        // send message to client based on duration of motion detected
+            if (duration >= 5000) {
+                socket.emit("long motion");
+                console.log('long motion detected');
+            } else {
+                socket.emit("short motion");
+                console.log('short motion detected');
+            };
+        });
+
+    });
+    socket.on("turn motion sensor off", function() {
+            // turn motion off
+            console.log('motion sensor off');
+    });
+        
+    // if client requests to turn LED
+    socket.on("turn LED on", function() {
+        led.on();
+        console.log('LED on');
+    });
+    socket.on("turn LED off", function() {
+        led.off();
+        console.log('LED off');
+    });
+});
